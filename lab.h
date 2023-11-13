e6fe1a5ae86f77d17683b0cbdf9865ff2422e707
tinhomoreira <149550732+tinhomoreira@users.noreply.github.com>
2023-11-12 22:43:22 -0300

    Clone Projeto 1 (lab.h)
    Apenas clonando o Projeto 1

---
+++ lab.h
@@ -0,0 +1,14 @@
+#ifndef LAB_H
+#define LAB_H
+
+struct Task {
+    int priority;
+    char description[300];
+    char category[100];
+};
+
+void cadastrarTarefa(struct Task tasks[], int *numTasks);
+void listarTarefas(struct Task tasks[], int numTasks);
+void deletarTarefa(struct Task tasks[], int *numTasks, int index);
+
+#endif
