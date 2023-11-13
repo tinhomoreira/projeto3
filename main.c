18894992fdc7333450ac2e6fcc888c9ad6d0c352
tinhomoreira <149550732+tinhomoreira@users.noreply.github.com>
2023-11-12 22:44:04 -0300

    Clone Projeto 1 (main.c)
    Apenas clonando código Projeto 1

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "lab.h" 
+
+int main() {
+    struct Task tasks[100];
+    int numTasks = 0;
+
+    int opcao;
+    do {
+        printf("==(¬‿¬)== Gerenciador de Tarefas ==(¬‿¬)==\n");
+        printf("1. Cadastrar Tarefa :)\n");
+        printf("2. Listar Tarefas\n");
+        printf("3. Deletar Tarefa :(\n");
+        printf("0. Sair ;(\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrarTarefa(tasks, &numTasks);
+                break;
+            case 2:
+                listarTarefas(tasks, numTasks);
+                break;
+            case 3: {
+                int index;
+                printf("Digite o índice da tarefa a ser deletada: ");
+                scanf("%d", &index);
+                deletarTarefa(tasks, &numTasks, index - 1);
+                break;
+            }
+            case 0:
+                printf("Encerrando o programa.\n");
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 0);
+
+    return 0;
+}
